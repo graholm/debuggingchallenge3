8c355f327d5b76e508480f562029d071825ba8fc
graholm <none>
2016-09-21 08:46:12 -0700

    Classadoo autocommit

--- script.js
+++ script.js
@@ -4,20 +4,20 @@ setInterval(function() {
     $("#blue-blinker").css({background: "white"});
     backgroundIsBlue = false;
   } else {		
-    $("#blue-blinker").css({background: "blue"});	
+    $("#blue-blinker").css({background: "lightblue"});	
     backgroundIsBlue = true;
   }	
 }, 500)
 
 
-var borderblinker = false
+var borderisshowing = false
 setInterval(function() {
   if (borderisshowing) {
-    $("#border-blinker").css({background: "white"});
-    backgroundisshowing = false;
+    $("#border-blinker").css({border: "0px solid black"});
+    borderisshowing = false;
   } else {		
-    $("#border-blinker").css({background: "blue"});	
-    backgroundIsBlue = true;
+    $("#border-blinker").css({border: "3px solid black"});	
+   borderisshowing = true;
   }	
 }, 500)
 
